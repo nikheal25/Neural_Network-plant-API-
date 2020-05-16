0dfeb95cd2b205bf9ff5dcef55208e7b6267cd16
Nick <nikheal25@gmail.com>
2020-05-16 21:59:31 +1000

    Working great

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const asparagusNN = require("./neuralNetworks");
 const neuralNet = require("./generalNeuralNetwork");
 const app = express();
 const port = process.env.PORT || 3000;
